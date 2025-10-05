2b559583fc079311819ff08e1c47af754862f271
Simon <simonhrsun@gmail.com>
2025-10-05 20:09:25 +0200

    version number 1.4.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = 'dash-molstar'
 copyright = '2025, Simon Sun'
 author = 'Simon Sun'
-release = '1.3.1'
+release = '1.4.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
