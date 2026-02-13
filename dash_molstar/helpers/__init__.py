371f0197f1ea593ca4797e00da14b5de50c2193a
Simon <simonhrsun@gmail.com>
2026-02-13 23:55:24 +0100

    improve helper imports

---
+++ dash_molstar/helpers/__init__.py
@@ -0,0 +1,8 @@
+# New API: from dash_molstar.helpers import Camera, Target, Representation, shapes
+# Also re-exports molstar_helper functions for convenience
+
+from ..utils.target import Target
+from ..utils.representations import Representation
+from ..utils.camera import Camera
+from ..utils import shapes
+from .molstar_helper import *
--- dash_molstar/utils/__init__.py
+++ dash_molstar/utils/__init__.py
@@ -1,1 +1,10 @@
-from . import molstar_helper
+# Legacy API (for backward compatibility): from dash_molstar.utils import molstar_helper
+# Recommended: from dash_molstar.helpers import Camera, Target, Representation, shapes
+
+from .target import Target
+from .representations import Representation
+from .camera import Camera
+from . import shapes
+
+# Re-export molstar_helper for backward compatibility
+from ..helpers import molstar_helper
